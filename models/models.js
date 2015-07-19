d2e11f34a48fc3c7e0d5842e78fc5236f0bd8ada
MiguelSarsa <miguel.sarsa@gmail.com>
2015-07-20 01:22:15 +0200

    Lista de Preguntas
    Se introduce un listado de preguntas

--- models/models.js
+++ models/models.js
@@ -27,18 +27,21 @@ var sequelize = new Sequelize(DB_name, user, pwd,
 );
 
 // Importar la definición de la tabla Quiz en quiz.js
-var Quiz = sequelize.import(path.join(__dirname, 'quiz'));
+var quiz_path = path.join(__dirname,'quiz');
+var Quiz = sequelize.import(quiz_path);
+
+//var Quiz = sequelize.import(path.join(__dirname, 'quiz');
 
 exports.Quiz = Quiz; // exportar definición de tabla Quiz
 
 // sequelize.sync() crea e inicializa la tabla de preguntas de la BBDD
-sequelize.sync().success(function(){
-  // success(..) ejecuta el manejador una vez creada la tabla
-  Quiz.count().success(function (count){
+sequelize.sync().then(function(){
+  // then(..) ejecuta el manejador una vez creada la tabla
+  Quiz.count().then(function (count){
     if(count === 0){    // la tabla se inicializa sólo si está vacía
-      Quiz.create({ pregunta: 'Capital de Italia',
-                    respuesta: 'Roma'
-				 }).success(function(){ console.log('Base de datos inicializada')});
+      Quiz.create({ pregunta: 'Capital de Italia?', respuesta: 'Roma'});
+      Quiz.create({ pregunta: 'Capital de Portugal?', respuesta: 'Lisboa'})
+      .then(function(){console.log('Base de datos inicializada');});
     };
   });
 });
