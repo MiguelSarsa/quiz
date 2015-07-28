8f6ce3389a535f8283846d5338c1d7e5e5fd54e2
MiguelSarsa <miguel.sarsa@gmail.com>
2015-07-28 21:49:06 +0200

    Validar Preguntas
    Se valida las preguntas, para saber si hay campos vacíos

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -19,20 +19,20 @@ exports.index = function(req, res, next) {
 	search = search.replace(" ", "%");
 
 	models.Quiz.findAll({ where: ['pregunta like ?', search ], order: 'pregunta' }).then(function(quizes) {
-		res.render('quizes/index', { quizes: quizes });
+		res.render('quizes/index', { quizes: quizes,  errors: [] });
 	}).catch(function(error) {next(error);})
 };
 
 // GET /quizes/:id
 exports.show = function(req, res){
-    res.render('quizes/show', {quiz: req.quiz});
+    res.render('quizes/show', {quiz: req.quiz,  errors: [] });
 };
 
 // GET /quizes/:id/answer
 exports.answer = function(req, res) {
   var resultado = 'Incorrecto';
   if (req.query.respuesta === req.quiz.respuesta) { resultado = 'Correcto';}
-  res.render('quizes/answer', {quiz: req.quiz, respuesta: resultado});
+  res.render('quizes/answer', {quiz: req.quiz, respuesta: resultado,  errors: [] });
 };
 
 // GET /quizes/new
@@ -41,15 +41,22 @@ exports.new = function(req, res){
     {pregunta: "", respuesta: ""}
   );
 
-  res.render('quizes/new', {quiz: quiz});
+  res.render('quizes/new', {quiz: quiz,  errors: [] });
 };
 
 // POST /quizes/create
 exports.create = function(req, res){
   var quiz = models.Quiz.build( req.body.quiz );
 
-  // guardar en BBDD los campos pregunta y respuesta de quiz
-  quiz.save({fields: ["pregunta", "respuesta"]}).then(function(){
-    res.redirect('/quizes');
-  }); // Redirección HTTP (URL relativo) lista de preguntas
+  quiz // guardar en BBDD los campos pregunta y respuesta de quiz
+  .validate()
+  .then(function(err){
+    if(err){
+      res.render('quizes/new', {quiz: quiz, errors: err.errors});
+    }else{
+      quiz.save({fields: ["pregunta", "respuesta"]}).then(function(){
+        res.redirect('/quizes');
+      }) // Redirección HTTP (URL relativo) lista de preguntas
+    }
+  });
 };
